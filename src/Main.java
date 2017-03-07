d9c9ef7ea9f5f2e8b2955eb3785197bf693d4689
Tristan Hollman <tristan.hollman@incentro.com>
2017-03-07 15:34:35 +0100

    Fixed Issue in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,27 @@
 // move refactoring ->  to src folder
 
-public class Main
-{
-	private String tijd;
-	private	 int uren;
-	private int minuten;
-
-	public Main()
-	{
-	}
-		public static void drukTijdAf(int hoursInNumber,int minutesInNumber)
-		{
-            System.out.println(ConvertTime.createConvertTime(hoursInNumber, minutesInNumber).convertTimeToString());
-		}
-
-    public static void main(String[]arg)
-		{
-			drukTijdAf(9, 15); //wordt: kwart over negen
-			drukTijdAf(0, 45);// wordt: kwart voor ��n
-			drukTijdAf(0, 15); //wordt: kwart over twaalf
-			drukTijdAf(0, 30); //wordt: kwart over twaalf
-			drukTijdAf(10, 30); //wordt: kwart over twaalf
-			drukTijdAf(9, 47); //wordt: kwart over twaalf
-			drukTijdAf(2, 43); //wordt: kwart over twaalf
-
-
-
-
-		}
-
-
+public class Main {
+    private String tijd;
+    private int uren;
+    private int minuten;
+    
+    public static void drukTijdAf(int hoursInNumber, int minutesInNumber) {
+        System.out.println(ConvertTime.createConvertTime(hoursInNumber, minutesInNumber).convertTimeToString());
+    }
+    
+    public static void main(String[] arg) {
+        drukTijdAf(9, 15); //wordt: kwart over negen
+        drukTijdAf(0, 45);// wordt: kwart voor ��n
+        drukTijdAf(0, 15); //wordt: kwart over twaalf
+        drukTijdAf(0, 30); //wordt: kwart over twaalf
+        drukTijdAf(10, 30); //wordt: kwart over twaalf
+        drukTijdAf(9, 47); //wordt: kwart over twaalf
+        drukTijdAf(2, 43); //wordt: kwart over twaalf
+        
+        
+    }
+    
+    
 }
 
 
