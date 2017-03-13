e3a9d5106b4bbed03d3a8fa099218337771ee502
Arne Heil <arne.heil@student.hu.nl>
2017-03-13 13:54:38 +0100

    const toegevoegd

--- src/ConvertTimeImp.java
+++ src/ConvertTimeImp.java
@@ -1,10 +1,14 @@
 //Extract interface
 
 public interface ConvertTimeImp {
-    String QUARTER_PAST = "kwart over"; //extract constant
-    String QUARTER_TO = "kwart voor"; //extract constant
-    String HALF_PAST = "half"; //extract constant
-    String O_CLOCK = "uur"; //extract constant
+    String KWART_OVER = "kwart over";
+    String QUARTER_PAST = KWART_OVER; //extract constant
+    String KWART_VOOR = "kwart voor";
+    String QUARTER_TO = KWART_VOOR; //extract constant
+    String HALF = "half";
+    String HALF_PAST = HALF; //extract constant
+    String UUR = "uur";
+    String O_CLOCK = UUR; //extract constant
 
     String convertTimeToString();
 
